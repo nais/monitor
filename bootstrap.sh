f3750b78d385f421ada8c801b7c078e30527d1e9
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2018-06-21 16:00:06 +0200

    Move chromium script to it's own file.

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,8 +9,6 @@ exec i3' > ~/.xinitrc
 
 mkdir -p ~/.config/i3/ && cp /etc/i3/config ~/.config/i3/config
 echo 'exec --no-startup-id xset s 00 && xset s noblank && xset s noexpose && xset dpms 0 0 0' >> ~/.config/i3/config
-echo '#!/usr/bin/env bash
-killall chromium-browser; /usr/bin/chromium-browser --ignore-certificate-errors --no-first-run --disable-web-resources --disable-sync --disable-prompt-on-repost --disable-default-apps --disable-background-networking --app="https://grafana.adeo.no/d/cZBgeYVmz/nais-prod-fss-single-page-cluster-dashboard?refresh=30s&orgId=1&kiosk"' > ~/chromium.sh && chmod +x ~/chromium.sh
 echo 'exec --no-startup-id ~/chromium.sh' >> ~/.config/i3/config
 
 echo '[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx' >> ~/.profile
