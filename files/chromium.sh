bd6bd2c3fe8c0f8e39390a515670b79596f485f4
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2018-06-22 08:29:33 +0200

    iframe fill screen

--- files/chromium.sh
+++ files/chromium.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 killall chromium-browser
-DISPLAY=:0.0 /usr/bin/chromium-browser --ignore-certificate-errors --no-first-run --disable-web-resources --disable-sync --disable-prompt-on-repost --disable-default-apps --disable-background-networking --app="https://nais.io/monitor?hostname=${hostname}"
+DISPLAY=:0.0 /usr/bin/chromium-browser --ignore-certificate-errors --no-first-run --disable-web-resources --disable-sync --disable-prompt-on-repost --disable-default-apps --disable-background-networking --app="https://nais.io/monitor?hostname=$(hostname)"
