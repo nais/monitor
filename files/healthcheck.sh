fb491e0a35606bd5a34b804982df212db136b10d
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2018-09-04 15:47:18 +0200

    Update screenMap and healthcheck

--- files/healthcheck.sh
+++ files/healthcheck.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 export DISPLAY=:0.0
 
-color_limit=740 # Normally around 100, white screen is ~750
+high_limit=740 # Normally around 100, white screen is ~750
+low_limit=50
 
 import -window root screen.png
 sum=`convert screen.png -resize 1x1 txt:- | grep -o "srgb\(.*\)" | grep -oP "\d+,\d+,\d+" | sed 's/,/+/g' | bc`
-if [[ ${sum} -gt ${color_limit} ]]; then
+if [[ ${sum} -gt ${high_limit} -or ${sum} -lt ${low_limit} ]]; then
 	echo "Screen too bright color (sum of rgb channels: ${sum}), something is probablt wrong. Pressing F5."
 	xdotool key F5
 else
