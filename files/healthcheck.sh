7dbc2a6bd1bd59c117e1210f90983817fda61c6b
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2018-09-21 14:26:46 +0200

    Updated healthcheck, removed output from cronjobs.

--- files/healthcheck.sh
+++ files/healthcheck.sh
@@ -6,9 +6,9 @@ low_limit=50
 
 import -window root screen.png
 sum=`convert screen.png -resize 1x1 txt:- | grep -o "srgb\(.*\)" | grep -oP "\d+,\d+,\d+" | sed 's/,/+/g' | bc`
-if [[ ${sum} -gt ${high_limit} -o ${sum} -lt ${low_limit} ]]; then
+if [[ ${sum} -gt ${high_limit} || ${sum} -lt ${low_limit} ]]; then
 	echo "Screen too bright color (sum of rgb channels: ${sum}), something is probablt wrong. Pressing F5."
-	xdotool key F5
+	killall chromium-browser
 else
 	echo "Everything looks perfect (sum of rgb channels: ${sum})."
 fi
