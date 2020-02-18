7455f3eb6f2b5a16061fe1e915b17673dc686190
Kim Tore Jensen <kimtjen@gmail.com>
2020-02-18 14:58:55 +0100

    fix multissh script

--- multissh.sh
+++ multissh.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #
 # Use tmux-cssh to connect to all hosts
-# https://github.com/dennishafemann/tmux-cssh
+# https://github.com/zinic/tmux-cssh
 #
 
 CSSH="cssh"
 
 inventory=`mktemp`
-grep -v '\[' inventory | awk '{print "pi@" $1}' > $inventory
+yq r inventory.yaml 'all.hosts.*.ansible_host' | awk '{print "pi@" $2}' > $inventory
 
 $CSSH -f $inventory
 
